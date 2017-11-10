4831d4832945c95c13be4123ad23630ea98b69ad
Kanika <kanikabinzani@gmail.com>
2017-11-09 21:42:03 -0500

    Added identifier

--- Code/client/Client.cs
+++ Code/client/Client.cs
@@ -13,11 +13,11 @@ using Newtonsoft.Json.Linq;
 namespace exam_portal{
     class Client
     {
-        public void send_data(string json)
+        public string send_data(string json, string key)
         {
-            var httpWebRequest = getConnection();
+            var httpWebRequest = getConnection(key);
             //To be removed once this method is called from outside
-            json = "{\"user\":\"test\",\"password\":\"p123\"}";
+            //json = "{\"user\":\"test\",\"password\":\"p123\"}";
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
                 streamWriter.Write(json);
@@ -26,26 +26,26 @@ namespace exam_portal{
                 Console.WriteLine("Data Transmitted: " + json);
             }
             string response = get_response(httpWebRequest);
+            return response;
         }
 
-        public HttpWebRequest getConnection()
+        private HttpWebRequest getConnection(string key)
         {
             TcpClient tcpclnt = new TcpClient();
             Console.WriteLine("Connecting.....");
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://exam-portal-kanika14.c9users.io:8081");
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://exam-portal-kanika14.c9users.io:8081?key="+key);
             httpWebRequest.ContentType = "application/json; charset=utf-8";
             httpWebRequest.Method = "POST";
             Console.WriteLine("Connected to Server.");
             return httpWebRequest;   
         }
 
-        public string get_response(HttpWebRequest httpWebRequest)
+        private string get_response(HttpWebRequest httpWebRequest)
         {
             var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             string response = "";
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                //var result = streamReader.ReadToEnd();
                 response = streamReader.ReadToEnd();
                 Console.WriteLine("Response from Server: " + response);
             }
