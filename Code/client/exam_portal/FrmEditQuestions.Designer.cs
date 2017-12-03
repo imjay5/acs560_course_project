89383ad4b734cd2dc79f464aff94709b882b9a38
Kanika <kanikabinzani@gmail.com>
2017-12-03 14:16:32 -0500

    Added layout changes

--- Code/client/exam_portal/FrmEditQuestions.Designer.cs
+++ Code/client/exam_portal/FrmEditQuestions.Designer.cs
@@ -45,9 +45,10 @@
             this.btnDeleteQuestion.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnDeleteQuestion.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnDeleteQuestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDeleteQuestion.Location = new System.Drawing.Point(242, 497);
+            this.btnDeleteQuestion.Location = new System.Drawing.Point(272, 621);
+            this.btnDeleteQuestion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDeleteQuestion.Name = "btnDeleteQuestion";
-            this.btnDeleteQuestion.Size = new System.Drawing.Size(191, 24);
+            this.btnDeleteQuestion.Size = new System.Drawing.Size(215, 30);
             this.btnDeleteQuestion.TabIndex = 1;
             this.btnDeleteQuestion.Text = "Delete Selected Question";
             this.btnDeleteQuestion.UseVisualStyleBackColor = false;
@@ -59,9 +60,10 @@
             this.btnAddQuestion.Enabled = false;
             this.btnAddQuestion.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnAddQuestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAddQuestion.Location = new System.Drawing.Point(454, 497);
+            this.btnAddQuestion.Location = new System.Drawing.Point(511, 621);
+            this.btnAddQuestion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAddQuestion.Name = "btnAddQuestion";
-            this.btnAddQuestion.Size = new System.Drawing.Size(163, 24);
+            this.btnAddQuestion.Size = new System.Drawing.Size(183, 30);
             this.btnAddQuestion.TabIndex = 2;
             this.btnAddQuestion.Text = "Add New Question";
             this.btnAddQuestion.UseVisualStyleBackColor = false;
@@ -72,9 +74,10 @@
             this.btnEdit.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnEdit.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEdit.Location = new System.Drawing.Point(633, 497);
+            this.btnEdit.Location = new System.Drawing.Point(712, 621);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(195, 24);
+            this.btnEdit.Size = new System.Drawing.Size(219, 30);
             this.btnEdit.TabIndex = 3;
             this.btnEdit.Text = "Update Selected Question";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -85,9 +88,10 @@
             this.btnClose.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Location = new System.Drawing.Point(600, 559);
+            this.btnClose.Location = new System.Drawing.Point(675, 699);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(95, 24);
+            this.btnClose.Size = new System.Drawing.Size(107, 30);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -98,9 +102,10 @@
             this.btnSubmit.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSubmit.Location = new System.Drawing.Point(720, 559);
+            this.btnSubmit.Location = new System.Drawing.Point(810, 699);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(108, 24);
+            this.btnSubmit.Size = new System.Drawing.Size(122, 30);
             this.btnSubmit.TabIndex = 5;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = false;
@@ -111,9 +116,10 @@
             this.btnAddRow.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnAddRow.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnAddRow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAddRow.Location = new System.Drawing.Point(163, 455);
+            this.btnAddRow.Location = new System.Drawing.Point(183, 569);
+            this.btnAddRow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAddRow.Name = "btnAddRow";
-            this.btnAddRow.Size = new System.Drawing.Size(95, 24);
+            this.btnAddRow.Size = new System.Drawing.Size(107, 30);
             this.btnAddRow.TabIndex = 6;
             this.btnAddRow.Text = "Add Row";
             this.btnAddRow.UseVisualStyleBackColor = false;
@@ -124,9 +130,10 @@
             this.btnDeleteRow.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnDeleteRow.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
             this.btnDeleteRow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDeleteRow.Location = new System.Drawing.Point(264, 455);
+            this.btnDeleteRow.Location = new System.Drawing.Point(297, 569);
+            this.btnDeleteRow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDeleteRow.Name = "btnDeleteRow";
-            this.btnDeleteRow.Size = new System.Drawing.Size(95, 24);
+            this.btnDeleteRow.Size = new System.Drawing.Size(107, 30);
             this.btnDeleteRow.TabIndex = 7;
             this.btnDeleteRow.Text = "Delete Row";
             this.btnDeleteRow.UseVisualStyleBackColor = false;
@@ -135,9 +142,10 @@
             // pnlQuestions
             // 
             this.pnlQuestions.AutoScroll = true;
-            this.pnlQuestions.Location = new System.Drawing.Point(163, 236);
+            this.pnlQuestions.Location = new System.Drawing.Point(183, 295);
+            this.pnlQuestions.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pnlQuestions.Name = "pnlQuestions";
-            this.pnlQuestions.Size = new System.Drawing.Size(838, 213);
+            this.pnlQuestions.Size = new System.Drawing.Size(830, 241);
             this.pnlQuestions.TabIndex = 0;
             // 
             // lblEditExam
@@ -145,18 +153,18 @@
             this.lblEditExam.AutoSize = true;
             this.lblEditExam.BackColor = System.Drawing.Color.Transparent;
             this.lblEditExam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEditExam.Location = new System.Drawing.Point(482, 185);
+            this.lblEditExam.Location = new System.Drawing.Point(542, 231);
             this.lblEditExam.Name = "lblEditExam";
-            this.lblEditExam.Size = new System.Drawing.Size(109, 25);
+            this.lblEditExam.Size = new System.Drawing.Size(130, 29);
             this.lblEditExam.TabIndex = 8;
             this.lblEditExam.Text = "Edit Exam";
             // 
             // frmEditQuestions
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1278, 594);
+            this.ClientSize = new System.Drawing.Size(1078, 742);
             this.Controls.Add(this.lblEditExam);
             this.Controls.Add(this.btnDeleteRow);
             this.Controls.Add(this.btnAddRow);
@@ -166,6 +174,7 @@
             this.Controls.Add(this.btnAddQuestion);
             this.Controls.Add(this.btnDeleteQuestion);
             this.Controls.Add(this.pnlQuestions);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmEditQuestions";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmEditQuestions";
