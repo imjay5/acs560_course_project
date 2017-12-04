b2594a06a6d51315a2cc9072a5db20a64c25b7f6
Kanika <kanikabinzani@gmail.com>
2017-12-04 09:32:02 -0500

    Added layout changes

--- Code/client/exam_portal/FrmReport.cs
+++ Code/client/exam_portal/FrmReport.cs
@@ -33,40 +33,52 @@ namespace exam_portal
             int lblY = 36;
             
             Label lblExamId = new Label();
-            lblExamId.Text = "Exam ";
+            lblExamId.Text = "#";
             lblExamId.Location = new Point(44, lblY);
-            lblExamId.Size = new Size(10, 20);
+            lblExamId.Size = new Size(40, 20);
             addDisplayProperties(lblExamId);
+            lblExamId.ForeColor = Color.Teal;
+            lblExamId.Font = new Font("Consolas", 10, FontStyle.Underline);
 
             Label lblExamTitle = new Label();
             lblExamTitle.Text = "Exam Title";
-            lblExamTitle.Location = new Point(60, lblY);
+            lblExamTitle.Location = new Point(90, lblY);
             lblExamTitle.Size = new Size(180, 20);
             addDisplayProperties(lblExamTitle);
+            lblExamTitle.ForeColor = Color.Teal;
+            lblExamTitle.Font = new Font("Consolas", 10, FontStyle.Underline);
 
-            Label lblNoQuestions = new Label();
+            /*Label lblNoQuestions = new Label();
             lblNoQuestions.Text = "# questions";
             lblNoQuestions.Location = new Point(250, lblY);
             lblNoQuestions.Size = new Size(30, 20);
             addDisplayProperties(lblNoQuestions);
+            lblExamId.ForeColor = Color.Teal;
+            lblExamId.Font = new Font("Consolas", 10, FontStyle.Underline);*/
 
             Label lblGradeA = new Label();
-            lblGradeA.Text = "# Grade A";
-            lblGradeA.Location = new Point(290, lblY);
-            lblGradeA.Size = new Size(30, 20);
+            lblGradeA.Text = "#Grade A";
+            lblGradeA.Location = new Point(280, lblY);
+            lblGradeA.Size = new Size(78, 20);
             addDisplayProperties(lblGradeA);
+            lblGradeA.ForeColor = Color.Teal;
+            lblGradeA.Font = new Font("Consolas", 10, FontStyle.Underline);
 
             Label lblGradeB = new Label();
-            lblGradeB.Text = "# Grade B";
-            lblGradeB.Location = new Point(330, lblY);
-            lblGradeB.Size = new Size(30, 20);
+            lblGradeB.Text = "#Grade B";
+            lblGradeB.Location = new Point(370, lblY);
+            lblGradeB.Size = new Size(78, 20);
             addDisplayProperties(lblGradeB);
+            lblGradeB.ForeColor = Color.Teal;
+            lblGradeB.Font = new Font("Consolas", 10, FontStyle.Underline);
 
             Label lblGradeC = new Label();
-            lblGradeC.Text = "# Grade C";
-            lblGradeC.Location = new Point(370, lblY);
-            lblGradeC.Size = new Size(30, 20);
+            lblGradeC.Text = "#Grade C";
+            lblGradeC.Location = new Point(460, lblY);
+            lblGradeC.Size = new Size(78, 20);
             addDisplayProperties(lblGradeC);
+            lblGradeC.ForeColor = Color.Teal;
+            lblGradeC.Font = new Font("Consolas", 10, FontStyle.Underline);
 
             for (int i = 0; i < examsList.Count(); i++)
             {
@@ -75,37 +87,37 @@ namespace exam_portal
                 Label lblExamIdValue = new Label();
                 lblExamIdValue.Text = (i + 1).ToString();
                 lblExamIdValue.Location = new Point(44, lblY);
-                lblExamIdValue.Size = new Size(10, 20);
+                lblExamIdValue.Size = new Size(40, 20);
                 addDisplayProperties(lblExamIdValue);
 
                 Label lblExamTitleValue = new Label();
                 lblExamTitleValue.Text = examsList[i].exam_title.ToString();
-                lblExamTitleValue.Location = new Point(60, lblY);
+                lblExamTitleValue.Location = new Point(90, lblY);
                 lblExamTitleValue.Size = new Size(180, 20);
                 addDisplayProperties(lblExamTitleValue);
 
-                Label lblNoQuestionsValue = new Label();
+                /*Label lblNoQuestionsValue = new Label();
                 lblNoQuestionsValue.Text = examsList[i].no_of_question.ToString();
                 lblNoQuestionsValue.Location = new Point(250, lblY);
                 lblNoQuestionsValue.Size = new Size(30, 20);
-                addDisplayProperties(lblNoQuestionsValue);
+                addDisplayProperties(lblNoQuestionsValue);*/
 
                 Label lblGradeAValue = new Label();
                 lblGradeAValue.Text = examsList[i].gradeA.ToString();
-                lblGradeAValue.Location = new Point(290, lblY);
-                lblGradeAValue.Size = new Size(30, 20);
+                lblGradeAValue.Location = new Point(280, lblY);
+                lblGradeAValue.Size = new Size(78, 20);
                 addDisplayProperties(lblGradeAValue);
 
                 Label lblGradeBValue = new Label();
                 lblGradeBValue.Text = examsList[i].gradeB.ToString();
-                lblGradeBValue.Location = new Point(330, lblY);
-                lblGradeBValue.Size = new Size(30, 20);
+                lblGradeBValue.Location = new Point(370, lblY);
+                lblGradeBValue.Size = new Size(78, 20);
                 addDisplayProperties(lblGradeBValue);
 
                 Label lblGradeCValue = new Label();
                 lblGradeCValue.Text = examsList[i].gradeC.ToString();
-                lblGradeCValue.Location = new Point(370, lblY);
-                lblGradeCValue.Size = new Size(30, 20);
+                lblGradeCValue.Location = new Point(460, lblY);
+                lblGradeCValue.Size = new Size(78, 20);
                 addDisplayProperties(lblGradeCValue);
             }
         }
